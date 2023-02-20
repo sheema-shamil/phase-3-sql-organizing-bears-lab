5510d1e45b451fbd49b01c24b91c60357c009134
sheema <sheemashamilfarook@gmail.com>
2023-02-20 19:43:55 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,alive) VALUES ("Mr. Chocolate",20,"M",true);
+INSERT INTO bears (name,age,sex,alive) VALUES ("Rowdy",20,"M",false);
+INSERT INTO bears (name,age,sex,alive) VALUES ("Tabitha",6,"F",true);
+INSERT INTO bears (name,age,sex,alive) VALUES ("Sergeant Brown",20,"M",false);
+INSERT INTO bears (name,age,sex,alive) VALUES ("Melissa",13,"F",true);
+INSERT INTO bears (name,age,sex,alive) VALUES ("Grinch",20,"M",false);
+INSERT INTO bears (name,age,sex,alive) VALUES ("Wendy",6,"M",true);
+INSERT INTO bears (name,age,sex,alive) VALUES (NULL,20,"F",false);
